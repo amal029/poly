d2f340b24c663efdcd0846cd87f4a4ef7d07f6b7
Avinash Malik <amal029@scss.tcd.ie>
2012-10-20 14:25:12 +0100

    Fixed the loop interchange pass. Can still make it much better

--- src/Vectorization/loopInterchange.ml
+++ src/Vectorization/loopInterchange.ml
@@ -9,9 +9,6 @@ module List = Batteries.List
 type loop_type =
   | PAR of symbol * simpleExpr * (int * int)
 
-let build_par stmt = function
-  | PAR (symbol,simpleExpr,lc) -> Par (symbol,simpleExpr,Block ([stmt],lc),lc)
-
 let get_symbol = function
   | Symbol (x,_) -> x
 
@@ -45,7 +42,7 @@ let is_independent_expr x = function
   | _ -> raise (Internal_compiler_error "Got a wrong colong expression")
 
 let is_independent my_stack = function
-  | For (s,x,_,_) -> 
+  | For (s,x,_,_) | Par (s,x,_,_) -> 
     (let ll = List.map (fun y -> is_independent_expr y x) my_stack in List.fold_left (&&) true ll)
     && (let ll = List.map (check s) my_stack in List.fold_left (&&) true ll)
   | CaseDef (c,_) -> 
@@ -55,42 +52,63 @@ let is_independent my_stack = function
     List.fold_left (&&) true ll
   | _ -> raise (Internal_compiler_error "Got a non For/CaseDef stmt when performing loop interchange!!")
 
+let build_par stmt = function
+  | PAR (symbol,simpleExpr,lc) -> Par (symbol,simpleExpr,Block ([stmt],lc), lc)
+
 let build_pars stmt pars = 
   let st = ref stmt  in
   let () = List.iter (fun x -> st := build_par !st x) (List.rev pars) in 
   !st
 
+let is_only_loops = function
+  | Block (x,_) -> List.for_all (fun x -> (match x with | Par _ | For _ -> true | _ -> false)) x
+  | Par _ | For _ -> true
+
 let rec process_stmt my_stack = function
   | For (symbol,simpleExpr,stmt,lc) as s ->
-    if (is_independent my_stack s) then For (symbol, simpleExpr, (process_stmt my_stack stmt), lc)
+    (* If there are only par and for statements internally then
+       process, else just build the par statments and give it back *)
+    if is_only_loops stmt then
+      if ((List.length my_stack = 0) || is_independent my_stack s) then 
+	For (symbol, simpleExpr, (process_stmt my_stack stmt), lc)
+      else For (symbol, simpleExpr, (build_pars stmt my_stack) , lc)
+    else For (symbol, simpleExpr, (build_pars stmt my_stack) , lc)
+    (*   else build_pars s my_stack *)
+    (* else build_pars s my_stack *)
+      
+  | Par (symbol,simpleExpr,stmt,lc) as s ->
+    (* If there are only par and for statements internally then
+       process, else just build the par statments and give it back *)
+    if is_only_loops stmt then
+      let () = IFDEF DEBUG THEN print_endline "Testing if Par is independent" ELSE () ENDIF in
+      if ((List.length my_stack = 0) || is_independent my_stack s) then
+	let () = IFDEF DEBUG THEN print_endline "Par is independent and hence will be interchanged" ELSE () ENDIF in
+	process_stmt ((PAR (symbol,simpleExpr,lc)) :: my_stack) stmt
+      else build_pars s my_stack
     else build_pars s my_stack
-    
-  | Par (symbol,simpleExpr,stmt,lc) -> 
-     process_stmt ((PAR (symbol,simpleExpr,lc)) :: my_stack) stmt
 
   | Block (x,lc) -> Block (List.map (process_stmt my_stack) x, lc)
 
-  | Split (x,lc) -> Split (process_stmt my_stack x, lc)
+  | _ as s -> raise (Internal_compiler_error ("Hit a non loop node when performing loop interchange: " ^ (Dot.dot_stmt s)))
 
-  | CaseDef (x,lc) as s -> 
-    let ret = (is_independent my_stack s) in
-    let () = IFDEF DEBUG THEN print_endline ("CaseDef is independent of par varref: " ^ (string_of_bool ret)) ELSE () ENDIF in
-    if ret then CaseDef (process_case my_stack x, lc)
-    else build_pars s my_stack
+let rec process_filter_stmts = function
+  | Par _ as s -> 
+    let () = IFDEF DEBUG THEN print_endline ("Trying to interchange: " ^ (Dot.dot_stmt s)) ELSE () ENDIF in
+    process_stmt [] s
+  | For (x,y,z,lc) -> For (x,y, process_filter_stmts z, lc)
+  | Block (x,lc) -> Block (List.map process_filter_stmts x,lc)
+  | Split (x,lc) -> Split (process_filter_stmts x, lc)
+  | CaseDef (x,lc) -> CaseDef (process_case x, lc)
+  | _ as s -> s
+and process_case = function
+  | Case (ll,o,lc) -> Case (List.map (process_clause) ll, process_otherwise o, lc)
+and process_clause = function
+  | Clause (r,s,lc) -> Clause (r,process_filter_stmts s, lc)
+and process_otherwise = function
+  | Otherwise (x,lc) -> Otherwise (process_filter_stmts x, lc)
 
-  | _ as s -> build_pars s my_stack
-
-and process_case my_stack = function
-  | Case (ll,o,lc) -> Case (List.map (process_clause my_stack) ll, process_other my_stack o, lc)
-
-and process_clause my_stack = function
-  | Clause (r,stmt,lc) -> Clause (r,process_stmt my_stack stmt,lc)
-
-and process_other my_stack = function
-  | Otherwise (x,lc) -> Otherwise (process_stmt my_stack x, lc) 
-
-let process_filter my_stack = function
-  | Filter (x,y,z,s) -> Filter (x,y,z,process_stmt my_stack s)
+let process_filter = function
+  | Filter (x,y,z,s) -> Filter (x,y,z,process_filter_stmts s)
 
 let rec interchange = function
-  | FCFG.Node (e,f,r,fl) -> FCFG.Node (e,process_filter [] f,r,(List.map interchange fl))
+  | FCFG.Node (e,f,r,fl) -> FCFG.Node (e,process_filter f,r,(List.map interchange fl))
